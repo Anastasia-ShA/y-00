36200484a56af90bf2941d56654f23383909b018
Anastasia-ShA <142487424+Anastasia-ShA@users.noreply.github.com>
2025-04-29 15:39:47 +0300

    Add files via upload

---
+++ korny_isparvlen/korny_isparvlen/kor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <locale>
+#include <malloc.h>
+#include <cmath>
+#include "korrren.h"
+using namespace std;
+
+int main() {
+	setlocale(0, "Russian");
+	korny();
+	return 0;
+}
