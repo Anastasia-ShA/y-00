fb4cd42a9d8ee8deb8ed6a374b561e27c2be5daf
Anastasia-ShA <142487424+Anastasia-ShA@users.noreply.github.com>
2025-04-29 14:27:22 +0300

    Add files via upload

---
+++ kombinatoryka_ispravlennaya/kombinatoryka_ispravlennaya/func1.cpp
@@ -0,0 +1,14 @@
+#include "kooombi.h"
+#include <map>
+#include <cstring>
+using namespace std;
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include "kooombi.h"
+
+int main() {
+	setlocale(0, "Russian");
+	kombinator();
+	return 0;
+}
